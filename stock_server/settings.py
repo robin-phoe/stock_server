65fe25250936df2c9c28e6c3cfac1192d7f29aec
robin <812121052@qq.com>
2021-09-12 15:55:03 +0800

    'init_server'

--- stock_server/settings.py
+++ stock_server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #add app
+    'rest_framework',
+    'books',
 ]
 
 MIDDLEWARE = [
