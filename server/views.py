98976c194e1837b3501fe095de19fdd4f98990e6
robin <812121052@qq.com>
2021-09-27 14:16:12 +0800

    '增加耗时显示'

--- server/views.py
+++ server/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -23,7 +25,9 @@ def stock_k_line(request):
 
 #分时
 def stock_time_line(request):
+    time_line_start = datetime.datetime.now()
     response_json = util.time_line(request)
+    print('monitor耗时：', datetime.datetime.now() - time_line_start)
     return JsonResponse(response_json)
 
 #板块k_line
@@ -38,10 +42,14 @@ def bk_time_line(request):
 
 #algo_monitor
 def algo_monitor(request):
+    monitor_start = datetime.datetime.now()
     response_json = util.get_algo(request)
+    print('monitor耗时：',datetime.datetime.now() - monitor_start)
     return JsonResponse(response_json)
 
 #取全日algo 分数
 def grade_all_day(request):
+    grade_start = datetime.datetime.now()
     response_json = util.get_grade_all_day(request)
+    print('monitor耗时：', datetime.datetime.now() - grade_start)
     return JsonResponse(response_json)
